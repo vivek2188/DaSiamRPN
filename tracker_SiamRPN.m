0c5b4df4d4afc67d31cc77e6f927869070fa9f87
Vivek Tiwari <tiwarivivekraj17@gmail.com>
2019-06-14 16:37:57 -0400

    tracker_SiamRPN modified

--- tracker_SiamRPN.m
+++ tracker_SiamRPN.m
@@ -1,6 +1,9 @@
 tracker_label = ['SiamRPN'];
 
-tracker_command = generate_python_command('vot_SiamRPN', {'/PATH/TO/CODE/FOLDER/'});
+tracker_command = generate_python_command('vot_SiamRPN', ...  % python script
+    {'/export/livia/home/vision/vtiwari/Project/DaSiamRPNWithoutMess/code', ... % path to the script
+    '/export/livia/home/vision/vtiwari/toolkit/trax/support/python'}); % supporting toolkit% supporting toolkit
 
 tracker_interpreter = 'python';
 
+tracker_linkpath = {'/export/livia/home/vision/vtiwari/toolkit/trax/build'};
